cf0813ffc8306d2fa28a0929e2cdb1eb6076cc62
Jenny Delaney <jennyelewis29@gmail.com>
2022-10-27 18:36:35 +0000

    Create get_customer_number function to get Customers telephone number for the order

--- run.py
+++ run.py
@@ -12,14 +12,28 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Bakery_cake_orders')
 
-def get_name_data():
+
+def get_customer_name():
     """
     Get Customers name ordering the cake
     """
     print("Welcome to Jessie's Bakery\n")
-    print("Please give a name for the cake order")
+    print("Please give a name for the cake order\n")
 
     name_str = input("Enter your name here:")
-    print(f"The name of the customer is {name_str}")
+    print(f"The name of the customer is {name_str}\n")
 
-get_name_data()
+
+def get_customer_number():
+    """
+    Get Customers telephone number for the order
+    """
+
+    print("Please confirm a contact telephone number for the order\n")
+    
+    phone_num_str = input("Enter your telephone number here:")
+    print(f"The telephone number given is {phone_num_str}\n")
+
+
+get_customer_name()
+get_customer_number()
