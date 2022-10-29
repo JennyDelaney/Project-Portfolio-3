9e34a481d8f6204f959b41042156ef33094a2117
Jenny Delaney <jennyelewis29@gmail.com>
2022-10-29 10:39:22 +0000

    Retrieve list of cake sizes available from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,7 +18,6 @@ def get_customer_name():
     """
     Get Customers name ordering the cake
     """
-    print("Welcome to Jessie's Bakery\n")
     print("Please give a name for the cake order\n")
 
     name_str = input("Enter your name here:")
@@ -28,7 +28,6 @@ def get_customer_number():
     """
     Get Customers telephone number for the order
     """
-
     print("Please confirm a contact telephone number for the order\n")
     
     phone_num_str = input("Enter your telephone number here:")
@@ -37,12 +36,26 @@ def get_customer_number():
 
 def number_cake_layers():
     print("How many layers would you like the cake to be?\n")
-    print("Please choose between - 1,2,3 or 4")
+    print("Please choose between - 1,2,3 or 4\n")
 
-    cake_layers_str = input("Please enter the number of layers you would like:")
-    print(f"You have chosen to have a {cake_layers_str} layered cake.")
+    cake_layers_str = input("Please enter the no. of layers you would like:")
+    print(f"You have chosen to have a {cake_layers_str} layered cake.\n")
 
 
-get_customer_name()
-get_customer_number()
-number_cake_layers()
+def cake_size():
+    print("You have the following options for the size of cake - ")
+    sizes = SHEET.worksheet("options").get_all_values()
+    sizes_data = sizes[0]
+    print(sizes_data)
+    
+def main():
+    """
+    Run all program functions
+    """ 
+    get_customer_name()
+    get_customer_number()
+    number_cake_layers()
+    cake_size()
+
+print("Welcome to Jessie's Bakery Ordering System\n")
+main()
