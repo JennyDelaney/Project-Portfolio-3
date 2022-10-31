fd950dbb1d3028a989751c0225f5bcd77c131090
Jenny Delaney <jennyelewis29@gmail.com>
2022-10-31 14:15:27 +0000

    Create confirmation of cake order string and pass variables between functions

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ from google.oauth2.service_account import Credentials
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+    "https://www.googleapis.com/auth/drive",
+]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Bakery_cake_orders')
+SHEET = GSPREAD_CLIENT.open("Bakery_cake_orders")
 
 
 def get_customer_name():
@@ -21,67 +21,78 @@ def get_customer_name():
 
     name_str = input("Enter your name here:")
     print(f"The name of the customer is {name_str}\n")
+    return name_str
 
 
-def get_customer_number():
+def get_customer_number(name):
     """
     Get Customers telephone number for the order
     """
-    print("Please confirm a contact telephone number for the order")
-    
+    print(f"{name}, please confirm a contact telephone number for the order")
+
     phone_num_str = input("Enter your telephone number here:")
     print(f"The telephone number given is {phone_num_str}\n")
+    return phone_num_str
 
 
-def number_cake_layers():
-    print("How many layers would you like the cake to be?\n")
+def number_cake_tiers(name):
+    """
+    Get the number of layers the customer would like the cake
+    """
+    print(f"{name}, how many layers would you like the cake to be?")
     print("Please choose between - 1,2,3 or 4")
 
-    cake_layers_str = input("Please enter the no. of layers you would like:")
-    print(f"You have chosen to have a {cake_layers_str} layered cake.\n")
+    cake_tiers_str = input("Please enter the no. of layers you would like:")
+    print(f"You have chosen to have a {cake_tiers_str} layered cake.\n")
+    return cake_tiers_str
 
 
-def cake_options():
+def cake_options(name, customer_number, number_tiers):
     """
-    Function to return the different options available from spreadsheet
+    Function to return the different options available for the cake
     """
-    print("You have the following options for the size of cake - ")
-
     options_ws = SHEET.worksheet("options").get_all_values()
+
+    # Confirmation of cake size
+    print("You have the following options for the size of cake - ")
     cake_sizes = [item[0] for item in options_ws]
     cake_sizes.pop(0)
     print(cake_sizes)
-    size_str = input('The cake size I would like is - ')
-    print(f"A {size_str} cake has been picked.\n")
+    size = input("The cake size I would like is option - ")
+    print(f"A {size} cake has been picked.\n")
 
+    # Confirmation of sponge flavour
     print("You have the following options for flavour of sponge - ")
     sponge_flavours = [item[1] for item in options_ws]
     sponge_flavours = sponge_flavours[1:]
     print(sponge_flavours)
-    sponge_flav_str = input('The sponge flavour I would like is - ')
+    sponge_flav_str = input("Choose the sponge flavour that you would like - ")
     print(f"You have picked a {sponge_flav_str} sponge flavour.\n")
-    
+
+    # Confirmation of filling
     print("Which type of filling would you like - ")
     filling_types = [item[2] for item in options_ws]
     del filling_types[0]
     del filling_types[2:]
     print(filling_types)
-    filling_str = input('The filling I would like is - ')
+    filling_str = input("The filling I would like is - ")
     print(f"You have picked a {filling_str} filling.\n")
 
-    filling_flavours = [item[3] for item in options_ws]
-    filling_flavours.pop(0)
-    print(filling_flavours)
-    
+    # Confirmation of cake order
+    print(f"{name}, {customer_number}: You have ordered the following cake - ")
+    print(
+        f"{number_tiers} tiers, {size} {sponge_flav_str} with {filling_str}"
+    )
+
 
 def main():
     """
     Run all program functions
-    """ 
-    get_customer_name()
-    get_customer_number()
-    number_cake_layers()
-    cake_options()
+    """
+    name = get_customer_name()
+    customer_number = get_customer_number(name)
+    number_tiers = number_cake_tiers(name)
+    cake_options(name, customer_number, number_tiers)
 
 
 print("Welcome to Jessie's Bakery Ordering System\n")
